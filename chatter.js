33512ef25e0351765e146e8fba065d98dd87fdc6
manhtai <vo@manhtai.com>
2016-11-15 18:47:13 +0700

    Add missing files

---
+++ chatter.js
@@ -0,0 +1,358 @@
+'use strict';
+
+const cowsay = require('cowsay');
+const cool   = require('cool-ascii-faces');
+
+const util   = require('./util');
+const dict   = require('./dictionary');
+const google = require('./google');
+const cow    = require('./cow');
+const config = require('./const');
+
+
+module.exports = (controller) => {
+
+    // Bypass tick log
+    controller.on('tick', () => {});
+
+
+    // Start conversation to save quote
+    controller.hears(['savequote', 'save quote'],
+        'direct_message,direct_mention,mention',
+        (bot, message) => {
+
+            controller.storage.users.get(message.user, (err, user) => {
+                // Some logs
+                if (user && user.quotes) {
+                    bot.reply(message, `You've had ${user.quotes.length} quotes so far. Save some more to make the the list longer!`);
+                } else {
+                    bot.reply(message, 'Yay, start saving awesome quotes now!');
+                }
+
+                const askContent = (response, convo) => {
+                    convo.ask('What is the new quote content?', (response, convo) => {
+                        convo.ask(`So here is your quote? \n\n *"${response.text}"*\n`, [
+                            {
+                                pattern: bot.utterances.yes,
+                                callback: (response, convo) => {
+                                    askAuthor(response, convo);
+                                    convo.next();
+                                }
+                            },
+                            {
+                                pattern: bot.utterances.no,
+                                callback: (response, convo) => {
+                                    convo.stop();
+                                }
+                            },
+                            {
+                                default: true,
+                                callback: (response, convo) => {
+                                    convo.repeat();
+                                    convo.next();
+                                }
+                            }
+                        ]);
+
+                        notifyComplete(response, convo);
+                        convo.next();
+
+                    }, {'key': 'content'});
+                };
+
+                const askAuthor = (response, convo) => {
+                    convo.ask('Okay now, who is the author then?', (response, convo) => {
+                        convo.ask(`Just for sure, the quote's author is \`${response.text}\`, right?`, [
+                            {
+                                pattern: bot.utterances.yes,
+                                callback: (response, convo) => {
+                                    convo.next();
+                                }
+                            },
+                            {
+                                pattern: bot.utterances.no,
+                                callback: (response, convo) => {
+                                    convo.stop();
+                                }
+                            },
+                            {
+                                default: true,
+                                callback: (response, convo) => {
+                                    convo.repeat();
+                                    convo.next();
+                                }
+                            }
+                        ]);
+
+                        convo.next();
+
+                    }, {'key': 'author'});
+                };
+
+                const notifyComplete = (response, convo) => {
+                    convo.on('end', function(convo) {
+                        if (convo.status == 'completed') {
+                            bot.reply(message, 'OK! I will save your quote to my book...');
+
+                            // Save to user data
+                            controller.storage.users.get(message.user, (err, user) => {
+                                if (!user) {
+                                    user = {
+                                        id: message.user,
+                                        quotes: []
+                                    };
+                                }
+                                user.quotes.push({
+                                    'content': convo.extractResponse('content'),
+                                    'author': convo.extractResponse('author')
+                                });
+                                controller.storage.users.save(user, (err, id) => {
+                                    bot.reply(message, `Got it. Now you had ${user.quotes.length} quotes written in my book.`);
+                                });
+                            });
+
+                            // Save to team data
+                            controller.storage.teams.get(message.team, (err, team_data) => {
+                                if (!team_data) {
+                                    team_data = {
+                                        id: message.team,
+                                        quotes: []
+                                    };
+                                }
+                                team_data.quotes.push({
+                                    'content': convo.extractResponse('content'),
+                                    'author': convo.extractResponse('author'),
+                                    'user': message.user
+                                });
+                                controller.storage.teams.save(team_data);
+                            });
+
+                        } else {
+                            // this happens if the conversation ended prematurely for some reason
+                            bot.reply(message, `OK, nevermind! ${cool()}`);
+                        }
+                    });
+                };
+
+                // Ask to save new quote
+                bot.startConversation(message, askContent);
+            });
+    });
+
+
+    // Show your quotes
+    controller.hears(
+        ['showmyquote', 'show my quote'],
+        'direct_message,direct_mention,mention',
+        (bot, message) => {
+            controller.storage.users.get(message.user, (err, user) => {
+                if (user && user.quotes) {
+                    let random = util.randomInt(0, user.quotes.length);
+                    bot.reply(message, util.formatQuote(user.quotes[random]));
+                } else {
+                    bot.reply(message, 'Hey there, you had no quotes to show!');
+                }
+            });
+    });
+
+
+    // Show quotes from your team
+    controller.hears(
+        ['showaquote', 'show a quote'],
+        'direct_message,direct_mention,mention,message_received',
+        (bot, message) => {
+            controller.storage.teams.get(message.team, (err, team_data) => {
+                if (team_data && team_data.quotes) {
+                    let random = util.randomInt(0, team_data.quotes.length);
+                    bot.reply(message, util.formatQuote(team_data.quotes[random]));
+                } else {
+                    bot.reply(message, 'Sorry there, I had no quotes to show!');
+                }
+            });
+    });
+
+
+    // https://github.com/piuccio/cowsay
+    controller.hears(
+        ['^(\\S+)say (.*)'],
+        'direct_message,direct_mention,mention',
+        (bot, message) => {
+            let animal = message.match[1];
+            const text = message.match[2];
+            cow.list((err, cows) => {
+                if (!err) {
+                    let found;
+                    for (let cow of cows) {
+                        if (cow === animal || cow === `cows/${animal}.cow`) {
+                            found = true;
+                            animal = cow;
+                            break;
+                        }
+                    }
+                    if (!found) {
+                        let random = util.randomInt(0, cows.length);
+                        animal = cows[random];
+                    }
+                    const cow = cowsay.say({ text: text, f: animal });
+                    bot.reply(
+                        message,
+                        '```\n' + cow + '\n```'
+                    );
+                }
+            });
+        }
+    );
+
+
+    //  https://github.com/google/google-api-nodejs-client
+    controller.hears(
+        ['^search(i|) (.*)'],
+        'direct_message,direct_mention,mention',
+        (bot, message) => {
+            const t = message.match[1] == 'i' ? 'image' : null;
+            const l = t == 'image' ? 'images' : 'things';
+            const q = message.match[2];
+            google.searchText(
+                q, t,
+                (err, resp) => {
+                    if (err) {
+                      return bot.botkit.log('An error occured', err);
+                    }
+                    if (resp.items && resp.items.length > 0) {
+                        let text = `Hey, I found ${resp.items.length} ${l} for you ${cool()}\n\n`;
+                        text += resp.items.map((item, i) => {
+                            let order = i + 1;
+                            return `${order}. *${item.title}* ~> ${item.link}`;
+                        }).join('\n');
+                        bot.reply(message, text);
+                    } else {
+                        bot.reply(message, 'Even Google can not find it, awesome!');
+                    }
+            });
+            bot.botkit.log(`User ${message.user} search: ${q}`);
+        }
+    );
+
+
+    // http://www.dictionaryapi.com/
+    controller.hears(
+        ['^say (.*)'],
+        'direct_message,direct_mention,mention',
+        (bot, message) => {
+            let word = message.match[1].trim();
+            dict.pronounceWord(word, (err, body) => {
+                try {
+                    let entry_list = body.entry_list;
+                    let result = [];
+                    if (entry_list.entry) {
+                        entry_list.entry.forEach((entry, index) => {
+                            if (util.isSame(entry.$.id, word)) {
+                                let r = `*${entry.hw[0]._ || entry.hw[0]}*`;
+                                let p = false;
+                                if (entry.fl) r += ` [${entry.fl[0]}]`;
+                                if (entry.pr) {
+                                    r += ` ~> \`${entry.pr[0]._ || entry.pr[0]}\``;
+                                    p = true;
+                                } else if (entry.altpr) {
+                                    r += ` ~> \`${entry.altpr[0]}\``;
+                                    p = true;
+                                }
+                                if (entry.sound) {
+                                    r += ` ~> ${dict.audioLink(entry.sound[0].wav[0])}`;
+                                }
+                                if (p) {
+                                    result.push(r);
+                                }
+                            }
+                        });
+                        result = result.join('\n');
+                    } else if (entry_list.suggestion) {
+                        entry_list.suggestion.forEach((suggestion, index) => {
+                            result.push(`\`${suggestion}\``);
+                        });
+                        result = `I can't find your word, but here are some suggestions: ${result.join(', ')}.`;
+                    }
+                    bot.reply(message, result);
+                } catch (e) {
+                    bot.reply(message, "I can't even speak it right, sorry!");
+                }
+            });
+        }
+    );
+
+
+    // Start conversation to shotdown me
+    controller.hears(['^shutdown$'],
+        'direct_message,direct_mention,mention',
+        (bot, message) => {
+
+            if (message.user == config.BOT_BOSS) {
+                bot.reply(message, "Goodbye, boss!");
+                process.exit();
+            } else {
+                const askAgain = (response, convo) => {
+                    convo.ask(`Why do you want me to die, won't you miss me?`, [
+                        {
+                            pattern: bot.utterances.yes,
+                            callback: (response, convo) => {
+                                bot.reply(message, `Yay, I know you do, so I won't leave you anytime soon then! ${cool()}`);
+                                convo.next();
+                            }
+                        },
+                        {
+                            pattern: bot.utterances.no,
+                            callback: (response, convo) => {
+                                bot.reply(message, `Opps, it's because we don't know much about each other yet, I'll stay so we can become friends! ${cool()}`);
+                                convo.next();
+                            }
+                        },
+                        {
+                            default: true,
+                            callback: (response, convo) => {
+                                convo.repeat();
+                                convo.next();
+                            }
+                        }
+                    ]);
+                    convo.next();
+                };
+
+                bot.startConversation(message, askAgain);
+        }
+    });
+
+
+    // Ơ, for Thu Thaor
+    controller.hears(
+        ['^ơ$'],
+        'direct_message,direct_mention,mention,message_received',
+        (bot, message) => {
+            bot.reply(message, message.text);
+    });
+
+
+    // Catch all
+    controller.hears(
+        ['.*'],
+        'direct_message,direct_mention,mention',
+        (bot, message) => {
+            bot.api.reactions.add({
+                timestamp: message.ts,
+                channel: message.channel,
+                name: 'heart',
+            }, (err, res) => {
+                if (err) {
+                    bot.botkit.log('Failed to add emoji reaction :(', err);
+                }
+            });
+            if (message.user == config.BOT_BOSS) {
+                bot.reply(message, "Hello, boss!");
+            } else {
+                bot.reply(
+                    message,
+                    cool() + " hi there!\n" +
+                    "I can help you to `search`, `say`, `cowsay`, `savequote`, `showaquote`, `shutdown` me and much more to come!");
+            }
+        }
+    );
+};
---
+++ whoisin.js
@@ -0,0 +1,247 @@
+'use strict';
+const config = require('./const');
+
+
+module.exports = (controller) => {
+    var myMessage;
+    controller.on('slash_command', function(bot, message) {
+        myMessage = message;  // replyPublicDelayed can't post to interactive message url so we have to hack this
+        switch (message.command) {
+            case '/ahem':
+
+                // Verify it
+                if (message.token !== config.verify_token) return; //just ignore it.
+
+                if (message.text === '' || message.text === 'help') {
+                    bot.replyPrivate(message, 'You know what to do, man!');
+                    return;
+                }
+
+                var lines = message.text.split('|').map(it => it.trim());
+                var text = lines[0];
+
+                // max 15 answers (3 for buttons, 1 for move to bottom, 15 for each answer)
+                if (lines.length > 16) {
+                    bot.replyPublic(message, `:sob: Sorry, you may only enter 15 options. Here is what you entered: ahem ${message.text}`);
+                    return;
+                }
+
+                // default actions incase the user doesn't specify one
+                var actions = [{
+                    name: 'answer',
+                    text: 'Yes',
+                    type: 'button',
+                    value: 'yes',
+                    style: 'default'
+                }, {
+                    name: 'answer',
+                    text: 'No',
+                    type: 'button',
+                    value: 'no',
+                    style: 'default'
+                }];
+
+                if (lines.length > 1) {
+                    actions = [];
+                    for (var i = 1; i < lines.length; i++) {
+                        var answer = lines[i];
+                        actions.push({
+                            name: 'answer',
+                            text: answer,
+                            type: 'button',
+                            value: answer,
+                            style: 'default'
+                        });
+                    }
+                }
+
+                // split the buttons into blocks of five if there are that many different
+                // questions
+                var attachments = [];
+                actions.forEach((action, num) => {
+                    let idx = Math.floor(num / 5);
+                    if (!attachments[idx]) {
+                        attachments[idx] = {
+                            text: '',
+                            fallback: message.text,
+                            callback_id: 'yes_or_no_callback',
+                            color: '#FF0000',
+                            actions: []
+                        };
+                    }
+                    attachments[idx].actions.push(action);
+                });
+
+                let bottomActions = [{
+                    name: 'recycle',
+                    text: ':arrow_down: kéo xuống',
+                    type: 'button'
+                }];
+
+                // move to the bottom button
+                attachments.push({
+                    text: '',
+                    fallback: 'move to the bottom',
+                    callback_id: 'yes_or_no_callback',
+                    actions: bottomActions
+                });
+
+
+                bot.replyPublic(message, {
+                    text: text,
+                    attachments: attachments
+                }, (err) => {
+                    if (err && err.message === 'channel_not_found') {
+                        bot.replyPrivate(message, 'Sorry, I can not write to a channel or group I am not a part of!');
+                    }
+                });
+
+                break;
+
+            default:
+                bot.replyPublic(message, 'I\'m afraid I don\'t know how to ' + message.command + ' yet.');
+
+        }
+    });
+
+    controller.on('interactive_message_callback', function(bot, message) {
+        if (message.callback_id == 'yes_or_no_callback') {
+            var orig = message.original_message;
+            switch (message.actions[0].name) {
+
+                case 'answer':
+                    var payload = JSON.parse(message.payload);
+                    var value = message.actions[0].value;
+                    var infoMsg = message.user + ' is ' + value;
+                    var username = payload.user.name;
+                    var foundExistingLine = false;
+                    orig.attachments = orig.attachments || [];
+
+                    var newAttachments = [];
+                    var lines = [];
+
+                    // look for an existing line/attachment and update it if found
+                    for (var i = 0; i < orig.attachments.length; i++) {
+                        var attachment = orig.attachments[i];
+
+                        if (attachment.actions) {
+                            newAttachments.push(attachment);
+                            continue;
+                        }
+
+                        // parse the attachment text and represent as an object
+                        var line = new AttachmentLine(attachment.text);
+                        if (line.answer === value) {
+                            foundExistingLine = true;
+                            line.add(username);
+                            lines.push(line);
+                        } else {
+                            line.remove(username);
+                            if (line.count() > 0) {
+                                lines.push(line);
+                            }
+                        }
+                    }
+
+                    // create a new line if next existing
+                    if (!foundExistingLine) {
+                        let line = new AttachmentLine();
+                        line.answer = value;
+                        line.add(username);
+                        lines.push(line);
+                    }
+
+                    // sort lines by most votes
+                    lines = lines.sort((a, b) => {
+                        return a.count() > b.count() ? -1 : 1;
+                    });
+
+                    // render and replace the updated attachments list
+                    orig.attachments = newAttachments.concat(lines.map((l) => {
+                        return {
+                            text: l.string(),
+                            mrkdwn_in: ["text"],
+                            color: '#47EEBC'
+                        };
+                    }));
+                    bot.replyInteractive(message, orig);
+                    break;
+
+                case 'recycle':
+                    var update = {
+                        text: 'Moved to bottom: ' + orig.text,
+                        delete_original: true
+                    };
+                    bot.replyInteractive(message, update, (err) => {
+                        console.log(err);
+                        bot.replyPublicDelayed(myMessage, orig);
+                    });
+                    break;
+
+                default:
+                    return;
+            }
+        }
+    });
+};
+
+
+function handleError(err, bot, message) {
+
+    console.error(err);
+
+    if (!message.response_url) return;
+
+    bot.reply({
+        text: `:scream: Uh Oh: ${err.message || err}`,
+        response_type: 'ephemeral',
+        replace_original: false
+    }, (err) => {
+        if (err) console.error('Error handling error:', err);
+    });
+}
+
+
+class AttachmentLine {
+
+    constructor(text) {
+        this.entries = [];
+        this.answer = '';
+        if (text) {
+            var parts = text.substring(text.indexOf(' ')).split(/»/);
+            parts = parts.map((it) => {
+                return it.trim();
+            });
+            this.answer = parts[0];
+            this.entries = parts[1].split(',').map((val) => {
+                return val.trim();
+            });
+        }
+    }
+
+    add(entry) {
+        this.remove(entry);
+        this.entries.push(entry);
+        return this;
+    }
+
+    remove(entry) {
+        this.entries = this.entries.filter((val) => {
+            return val !== entry;
+        });
+        return this;
+    }
+
+    contains(entry) {
+        return this.entries.indexOf(entry) > -1;
+    }
+
+    count() {
+        return this.entries.length;
+    }
+
+    string() {
+        let dots = '';
+        return '*' + this.count() + '*' + ' ' + this.answer + ' » ' + this.entries.join(', ');
+    }
+}
