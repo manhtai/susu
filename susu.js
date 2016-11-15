e6cd4b5046cef345fee1fdc8717c07a4eacaa5ec
manhtai <vo@manhtai.com>
2016-11-15 23:47:45 +0700

    Make listen explicit

--- susu.js
+++ susu.js
@@ -2,6 +2,8 @@
 
 const Botkit = require('botkit');
 const config = require('./const');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const controller = Botkit.slackbot({
     storage: config.mongoStorage,
@@ -19,20 +21,6 @@ controller.configureSlackApp(
 );
 
 
-controller.setupWebserver(config.port, function (err, webserver) {
-
-    webserver.get('/', (req, res) => { res.send('Hi, I am a bot!'); });
-
-    controller.createWebhookEndpoints(webserver);
-
-    controller.createOauthEndpoints(webserver, function (err, req, res) {
-        if (err) {
-            res.status(500).send('ERROR: ' + err);
-        } else {
-            res.send('Success!');
-        }
-    });
-});
 
 
 const bot = controller.spawn({
@@ -56,3 +44,28 @@ controller.on('rtm_close', () => {
 
 require('./chatter')(controller);
 require('./whoisin')(controller);
+
+
+// Setup server
+var static_dir =  __dirname + '/public';
+var webserver = express();
+webserver.use(bodyParser.json());
+webserver.use(bodyParser.urlencoded({ extended: true }));
+webserver.use(express.static(static_dir));
+
+webserver.get('/', (req, res) => { res.send('Hi, I am a bot!'); });
+
+controller.webserver = webserver;
+controller.config.port = config.port;
+
+controller.createWebhookEndpoints(webserver);
+
+controller.createOauthEndpoints(webserver, function (err, req, res) {
+    if (err) {
+        res.status(500).send('ERROR: ' + err);
+    } else {
+        res.send('Success!');
+    }
+});
+
+webserver.listen(config.port);
