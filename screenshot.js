66630d0e4152861c18004e05d3948c79c6fcc6c5
manhtai <vo@manhtai.com>
2017-10-07 11:23:52 +0700

    Get rid of jimp and file

--- screenshot.js
+++ screenshot.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const puppeteer = require('puppeteer');
 const crypto = require('crypto');
-const Jimp = require("jimp");
-
 
 const config = require('./const');
 
@@ -11,33 +9,27 @@ const router = express.Router();
 const sha256 = x => crypto.createHash('sha256').update(x, 'utf8').digest('hex');
 
 
-const getScreenShot = async (url) => {
+const getScreenShot = async (url, clip) => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    const path = `susu-screenshot-${sha256(url)}.png`;
 
+    page.setViewport({ width: 1024, height: 768 });
     await page.goto(url);
-    await page.screenshot({ path });
+    const buffer = await page.screenshot({ clip });
     await browser.close();
-    return path;
+    return buffer;
 };
 
 
 router.get('/', async (req, res) => {
     const url = req.query.url;
     const xywh = [req.query.x, req.query.y, req.query.w, req.query.h];
-    const [x, y, w, h] = xywh.map(i => !isNaN(i) ? parseInt(i): 0);
+    const [x, y, width, height] = xywh.map(i => !isNaN(i) ? parseInt(i): 0);
+    const clip = { x, y, width, height };
 
-    const path = await getScreenShot(url);
-    const image = await Jimp.read(path);
-
-    console.log(x, y, w, h);
-
-    image.crop(x, y, w, h).getBuffer(Jimp.MIME_PNG, (err, buffer) => {
-        if (err) return res.send(err);
-        res.type('png');
-        res.end(buffer, 'binary');
-    });
+    const buffer = await getScreenShot(url, clip);
+    res.type('png');
+    res.end(buffer, 'binary');
 });
 
 
