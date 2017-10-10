47d85e0d35f09a12ff9721f8cfa3aea1ed65ccec
manhtai <vo@manhtai.com>
2017-10-10 11:03:29 +0700

    Add timeout param for screenshot

--- screenshot.js
+++ screenshot.js
@@ -6,14 +6,16 @@ const config = require('./const');
 const router = express.Router();
 
 
-const getScreenShot = async (url, clip) => {
+const getScreenShot = async (url, clip, timeout) => {
     const browser = await puppeteer.launch(
         {args: ['--no-sandbox', '--disable-setuid-sandbox']}
     );
     const page = await browser.newPage();
 
-    page.setViewport({ width: 1024, height: 768 });
-    await page.goto(url);
+    page.setViewport({ width: 2048, height: 1536 });
+    await page.goto(url, {
+        waitUntil: "networkidle", networkIdleTimeout: timeout
+    });
     const buffer = await page.screenshot({ clip });
     await browser.close();
     return buffer;
@@ -22,11 +24,12 @@ const getScreenShot = async (url, clip) => {
 
 router.get('/', async (req, res) => {
     const url = req.query.url;
+    const timeout = req.query.t || 5000;
     const xywh = [req.query.x, req.query.y, req.query.w, req.query.h];
     const [x, y, width, height] = xywh.map(i => !isNaN(i) ? parseInt(i): 0);
     const clip = { x, y, width, height };
 
-    const buffer = await getScreenShot(url, clip);
+    const buffer = await getScreenShot(url, clip, timeout);
     res.type('png');
     res.end(buffer, 'binary');
 });
