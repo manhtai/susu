685a42627d5531ff8a07f9bba61ea2b2e5e71964
manhtai <vo@manhtai.com>
2017-10-16 22:58:36 +0700

    Fast response to screenshot url

--- screenshot.js
+++ screenshot.js
@@ -1,6 +1,11 @@
-const express = require('express');
-const util = require('./util');
+const express  = require('express');
+const fs       = require("fs");
+const path     = require("path");
+const crypto = require('crypto');
 
+const util     = require('./util');
+
+const temp_dir = path.join(process.cwd(), 'temp/');
 const router = express.Router();
 
 
@@ -11,9 +16,29 @@ router.get('/', async (req, res) => {
     const [x, y, width, height] = xywh.map(i => !isNaN(i) ? parseInt(i): 0);
     const clip = { x, y, width, height };
 
+    const filename = `${crypto.createHash('md5').update(req.url).digest("hex")}.png`;
+
+    // Send link to file
+    res.send(`Visit ${req.headers.host}/screenshot/${filename} to get the file later...`);
+
     const buffer = await (util.getScreenShot(url, clip, timeout));
-    res.type('png');
-    res.end(buffer, 'binary');
+
+    if (!fs.existsSync(temp_dir))
+        fs.mkdirSync(temp_dir);
+
+    // Write file to local
+    const filepath = path.resolve(temp_dir, filename);
+    fs.writeFile(filepath, buffer, (err) => {
+        if (err) console.log(`Error while writing file for url ${url}`, err);
+    });
+});
+
+
+router.get('/:filename', async (req, res) => {
+    const filename = req.params.filename;
+    const filepath = path.resolve(temp_dir, filename);
+    if (fs.existsSync(filepath)) res.sendFile(filepath);
+    else res.send('File not found! Please wait a little more or get the screenshot again.');
 });
 
 
